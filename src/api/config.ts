70a6b6e85cc4f021366009953f6d4cd93516676d
tramaunhan <xiaomir4b@gmail.com>
2026-06-25 14:20:04 +0700

    update

--- src/api/config.ts
+++ src/api/config.ts
@@ -162,8 +162,8 @@ const normalizeTripSeatMap = (data: TripSeatMapApiResponse): TripSeatMapResponse
 
 // Base API configuration
 const api = axios.create({
-    // baseURL: 'https://api.aihost.io.vn/api',
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://api.aihost.io.vn/api',
+    // baseURL: 'http://localhost:8080/api',
     timeout: 100000,
     headers: {
         'Content-Type': 'application/json',
